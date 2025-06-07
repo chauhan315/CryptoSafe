20e585fc883831290648f2c434fa3e98a49c100a
Ankur Singh Chauhan <ankur3382@outlook.com>
2025-06-07 22:26:45 +0530

    fourth commit, added the UI for the files view

--- CryptoSafe/src/main/java/controller/MainController.java
+++ CryptoSafe/src/main/java/controller/MainController.java
@@ -1,5 +1,38 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class MainController {
 
+	@FXML
+	private void handleSearch(ActionEvent event) {
+		// Your search logic here
+		System.out.println("Search button clicked!");
+	}
+
+	@FXML
+	private void handleAddEntry(ActionEvent event) {
+
+	}
+
+	@FXML
+	private void handleEditEntry(ActionEvent event) {
+
+	}
+
+	@FXML
+	private void handleDeleteEntry(ActionEvent event) {
+
+	}
+
+	@FXML
+	private void handleBackup(ActionEvent event) {
+
+	}
+	
+	@FXML
+	private void logout(ActionEvent event) {
+		
+	}
 }
