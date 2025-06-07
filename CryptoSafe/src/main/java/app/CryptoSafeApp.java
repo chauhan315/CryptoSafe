23b135db3abd45992243b3eaf71396e1e94ecea9
Ankur Singh Chauhan <ankur3382@outlook.com>
2025-06-07 23:25:06 +0530

    fifth commit, added the first time runnig check

--- CryptoSafe/src/main/java/app/CryptoSafeApp.java
+++ CryptoSafe/src/main/java/app/CryptoSafeApp.java
@@ -8,27 +8,33 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class CryptoSafeApp extends Application{
+public class CryptoSafeApp extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader;
+		
 		try {
-			FXMLLoader loader = new FXMLLoader(AppConfig.class.getResource("/view/entry_form.fxml"));
+			if (AppConfig.isFirstLaunch()) {
+				loader = new FXMLLoader(AppConfig.class.getResource("/view/setup_pin.fxml")); // First-time PIN setup
+			} else {
+				loader = new FXMLLoader(AppConfig.class.getResource("/view/login.fxml")); // Normal login
+			}
 			Scene scene = new Scene(loader.load());
 			scene.getStylesheets().add(AppConfig.class.getResource("/resources/styles.css").toExternalForm());
-			
+
 			primaryStage.setTitle("CryptoVault - Secure Vault");
 			primaryStage.setScene(scene);
 			primaryStage.getIcons().add(AppConfig.getAppIcon());
 			primaryStage.setResizable(true);
 			primaryStage.show();
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
---
+++ CryptoSafe/src/main/java/controller/SetupPinController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+
+public class SetupPinController {
+	
+	@FXML
+	private PasswordField pinField;
+	
+	@FXML
+	private PasswordField confirmPinField;
+	
+	@FXML
+	private Label errorLabel;
+	
+	
+	
+	
+	@FXML
+	private void handleSubmit(ActionEvent event) {
+		
+	}
+}
