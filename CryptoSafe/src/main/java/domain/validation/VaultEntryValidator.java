16c53582272512abd2671e027cc693434926bdf8
Ankur Singh Chauhan <ankur3382@outlook.com>
2025-06-13 22:59:21 +0530

    added the validation for entering metadata in VaultDatabase

--- CryptoSafe/src/main/java/domain/validation/VaultEntryValidator.java
+++ CryptoSafe/src/main/java/domain/validation/VaultEntryValidator.java
@@ -1,5 +1,16 @@
 package domain.validation;
 
+import domain.model.VaultEntry;
+
 public class VaultEntryValidator {
 
+	public Boolean validate(VaultEntry entry) {
+
+		if (entry.getCreatedAt() != null && entry.getTitle() != null && entry.getType() != null
+				&& entry.getUpdatedAt() != null && entry.getUsername() != null) {
+			return true;
+		}
+
+		return false;
+	}
 }
