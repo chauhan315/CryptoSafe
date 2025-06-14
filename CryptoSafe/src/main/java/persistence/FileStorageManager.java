ba0cf13cff198fb2001c47ff8f19803b520241a4
Ankur Singh Chauhan <ankur3382@outlook.com>
2025-06-14 20:22:27 +0530

    13th commit, added the method for saving encryptedfile

--- CryptoSafe/src/main/java/persistence/FileStorageManager.java
+++ CryptoSafe/src/main/java/persistence/FileStorageManager.java
@@ -1,5 +1,29 @@
 package persistence;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class FileStorageManager {
+
+	private static final String VAULT_DIR = "data/vault";
+	
+	static {
+		File vaultDir = new File(VAULT_DIR);
+		if(!vaultDir.exists()) vaultDir.mkdirs();
+	}
+
+	public static void saveEncryptedFile(File selectedFile, byte[] encryptedBytes) {
+
+		File targetFile = new File(VAULT_DIR, selectedFile.getName());
+		
+		try(FileOutputStream fos = new FileOutputStream(targetFile)){
+			fos.write(encryptedBytes);
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		} 
+	}
 
 }
